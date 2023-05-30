ace151cd3e4c3d1799ee7351627c5ce0cd943081
Ish <ish.mishra@gmail.com>
2023-05-30 22:55:30 +0100

    Git Commit
    Signed-off-by: Ish <ish.mishra@gmail.com>

---
+++ grade-submission/src/main/java/com/learn/gradesubmission/controller/GradeController.java
@@ -0,0 +1,41 @@
+package com.learn.gradesubmission.controller;
+
+
+import com.learn.gradesubmission.service.GradeService;
+import com.learn.gradesubmission.Grade;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GradeController {
+
+    @Autowired
+    GradeService gradeService = new GradeService();
+
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String id) {
+        model.addAttribute("grade", gradeService.getGradeById(id));
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(@Valid Grade grade, BindingResult result) {
+        if (result.hasErrors()) return "form";
+        gradeService.submitGrade(grade);
+        return "redirect:/grades";
+    }
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", gradeService.getGrades());
+        return "grades";
+    }
+
+
+}
